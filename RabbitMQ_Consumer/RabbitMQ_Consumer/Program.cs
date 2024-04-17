6883f2e1dbd12c0fa4b0c30681deb18dc435bcb1
Rufat <rufat@Rufats-MacBook-Air.local>
2024-04-18 02:42:10 +0400

    feat:I added RabbitMQ Publisher and RabbitMQ Consumer projects to my gitHub repo

---
+++ RabbitMQ_Consumer/RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//create connection
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("amqps://bqkozkbb:Pei9-L7Q68-ZwjrrWKGGK6Dz_i8jpUeW@stingray.rmq.cloudamqp.com/bqkozkbb");
+
+//create channel
+using IConnection connection= connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//create queue
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("example-queue", false, consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //e.Body.Span;
+    //e.Body.ToArray(); I can recieve message in byte array format
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.ReadLine();
+
+ 
---
+++ RabbitMQ_Publisher/RabbitMQ_Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+//create Connection
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("amqps://bqkozkbb:Pei9-L7Q68-ZwjrrWKGGK6Dz_i8jpUeW@stingray.rmq.cloudamqp.com/bqkozkbb");
+
+//create channel
+
+using IConnection connection= connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//create queue
+
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//send message to queue
+while (true)
+{
+    Console.WriteLine("Send Message:");
+    string message = Console.ReadLine();
+
+    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: messageBytes);
+}
